81f785a83d958629d4f951c879b15bfaf5c84de8
Matt Riemer <matthew@agilestyle.com>
2015-01-28 13:07:45 -0700

    read more snippet

--- app/helpers/effective_posts_helper.rb
+++ app/helpers/effective_posts_helper.rb
@@ -8,8 +8,23 @@ module EffectivePostsHelper
       "Published",
       "on #{post.published_at.strftime("%d-%b-%Y %l:%M %p")}",
       ("to #{link_to_post_category(post.category)}" if Array(EffectivePosts.categories).length > 0),
-      "by #{post.user.to_s.presence || 'Unknown'}"
+      ("by #{post.user.to_s.presence || 'Unknown'}" if EffectivePosts.post_meta_author)
     ].compact.join(' ').html_safe
+  end
+
+  def post_excerpt(post, options = {})
+    content = effective_region(post, :content) { "<p>Default content</p>".html_safe }
+
+    index = content.index(Effective::Snippets::ReadMoreDivider::TOKEN)
+
+    if index.present? # We have to return the excerpt and add a Read more... link
+      content[0...index].html_safe +
+      content_tag(:p, :class => 'post-read-more') do
+        link_to((options.delete(:label) || 'Read more...'), effective_post_path(post), options)
+      end
+    else
+      content
+    end
   end
 
   def link_to_post_category(category, options = {})
---
+++ app/models/effective/snippets/read_more_divider.rb
@@ -0,0 +1,9 @@
+module Effective
+  module Snippets
+    class ReadMoreDivider < Snippet
+      TOKEN = "<div style='display: none;'>READ_MORE_DIVIDER</div>"
+
+      attribute :throwaway, String
+    end
+  end
+end
