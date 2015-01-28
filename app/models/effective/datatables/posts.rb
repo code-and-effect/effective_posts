5bd4b893c61ba6ad21592685f54163a783e18f0d
Matt Riemer <matthew@agilestyle.com>
2015-01-28 13:21:26 -0700

    clean up datable

--- app/models/effective/datatables/posts.rb
+++ app/models/effective/datatables/posts.rb
@@ -2,13 +2,14 @@ if defined?(EffectiveDatatables)
   module Effective
     module Datatables
       class Posts < Effective::Datatable
+        table_column :published_at
+
         table_column :id
 
         table_column :title
         table_column :category, :filter => {:type => :select, :values => EffectivePosts.categories }
 
         table_column :draft
-        table_column :published_at
 
         table_column :actions, :sortable => false, :filter => false, :partial => '/admin/posts/actions'
 
