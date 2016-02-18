f467acc55ddc16dd14c5d6dd97f66021e9f6806a
Matt Riemer <matthew@agilestyle.com>
2016-02-18 10:44:27 -0700

    Add post_categories, render_post_categories. Use keyword args

--- app/helpers/effective_posts_helper.rb
+++ app/helpers/effective_posts_helper.rb
@@ -1,6 +1,11 @@
 require 'cgi'
 
 module EffectivePostsHelper
+  def effective_post_path(post)
+    category = post.category.to_s.downcase
+    EffectivePosts.use_category_routes ? "/#{category}/#{post.to_param}" : effective_posts.post_path(post, category: category.to_s)
+  end
+
   def render_post(post)
     render(partial: 'effective/posts/post', locals: { post: post })
   end
@@ -19,32 +24,36 @@ module EffectivePostsHelper
   # :omission => '...' passed to the final text node's truncate
   # :length => 200 to set the max inner_text length of the content
   # All other options are passed to the link_to 'Read more'
-  def post_excerpt(post, opts = {})
+  def post_excerpt(post, read_more_link: true, label: 'Read more', omission: '...', length: 200)
     content = effective_region(post, :content, :editable => false) { '<p>Default content</p>'.html_safe }
 
-    options = {
-      :label => 'Read more',
-      :omission => '...',
-      :length => 200
-    }.merge(opts)
-
     divider = content.index(Effective::Snippets::ReadMoreDivider::TOKEN)
-    length = options.delete(:length)
-    omission = options.delete(:omission)
+    read_more = (read_more_link && label.present?) ? readmore_link(post, label: label) : ''
 
     CGI.unescapeHTML(if divider.present?
-      truncate_html(content, Effective::Snippets::ReadMoreDivider::TOKEN, '') + readmore_link(post, options)
+      truncate_html(content, Effective::Snippets::ReadMoreDivider::TOKEN, '') + readmore
     elsif length.present?
-      truncate_html(content, length, omission) + readmore_link(post, options)
+      truncate_html(content, length, omission) + read_more
     else
       content
     end).html_safe
   end
 
-  def readmore_link(post, options)
+  def read_more_link(post, options)
     content_tag(:p, class: 'post-read-more') do
       link_to((options.delete(:label) || 'Read more'), effective_posts.post_path(post), options)
     end
+  end
+  alias_method :readmore_link, :read_more_link
+
+  ### Post Categories
+
+  def post_categories
+    categories = EffectivePosts.categories
+  end
+
+  def render_post_categories
+    render(partial: '/effective/posts/categories', locals: { categories: post_categories })
   end
 
   def link_to_post_category(category, options = {})
@@ -54,20 +63,14 @@ module EffectivePostsHelper
     link_to(category.to_s.titleize, href, options)
   end
 
-  def effective_post_path(post)
-    category = post.category.to_s.downcase
-    EffectivePosts.use_category_routes ? "/#{category}/#{post.to_param}" : effective_posts.post_path(post, category: category.to_s)
+  ### Recent Posts
+
+  def recent_posts(user: current_user, category: nil, limit: EffectivePosts.per_page)
+    @recent_posts ||= Effective::Post.posts(user: user, category: category).limit(limit)
   end
 
-  def render_post_categories
-    categories = EffectivePosts.categories
-    return nil unless categories.present?
-
-    render partial: '/effective/posts/categories', locals: { categories: categories }
-  end
-
-  def render_recent_posts(user: nil, category: nil, limit: 4)
-    posts = Effective::Post.posts(current_user, category).limit(limit)
+  def render_recent_posts(user: current_user, category: nil, limit: EffectivePosts.per_page)
+    posts = recent_posts(user: user, category: category, limit: limit)
 
     render partial: '/effective/posts/recent_posts', locals: { posts: posts }
   end
