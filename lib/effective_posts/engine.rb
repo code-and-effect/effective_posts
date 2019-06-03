108eabfc5f1478c840a119bc69582ad1973e661d
Matt Riemer <matthew@codeandeffect.com>
2019-06-03 09:23:47 -0600

    Use action_controller_base hook to load helper

--- lib/effective_posts/engine.rb
+++ lib/effective_posts/engine.rb
@@ -4,7 +4,7 @@ module EffectivePosts
 
     # Include Helpers to base application
     initializer 'effective_posts.action_controller' do |app|
-      ActiveSupport.on_load :action_controller do
+      ActiveSupport.on_load :action_controller_base do
         helper EffectivePostsHelper
       end
     end
